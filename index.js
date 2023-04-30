6ea298ec1e2c3d682e6838e85242b23f5c584475
Brian Laureano <brayantechgeek@gmail.com>
2023-04-30 18:21:02 -0400

    updated

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCase);
 }
+
+function titleCase(str) {
+  return str.split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+module.exports = {
+  tutorials,
+  titleCased
+};
